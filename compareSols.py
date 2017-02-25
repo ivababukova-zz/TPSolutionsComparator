b3656d6dd123acc84110a4385258f35b4f6057d9
Iva Babukova <iva.babukova@skyscanner.net>
2017-02-25 17:02:29 +0000

    Script for comparing solutions

--- compareSols.py
+++ compareSols.py
@@ -4,29 +4,41 @@ import sys
 def readSol(solfile):
     sols = []
     with open(solfile, "r") as f:
+        sol = []
         for line in f.readlines():
             line = line.split()
+            if len(line) == 0:
+                sols.append(sol)
+                sol = []
             if len(line) > 2 and line[0] == "Schedule:":
                 trip = []
                 for flight in line[1:]:
                     trip.append(int(flight))
-                sols.append(trip)
+                sol.append(trip)
             elif len(line) == 2:
-                sols.append(float(line[1]))
+                sol.append(float(line[1]))
     return sols
 
-def compare(sol1, sol2):
-    for i1, i2 in zip(sol1, sol2):
-        if not i1 == i2:
-            return(False, i1, i2)
-    return([True])
+def compare(sols1, sols2):
+    areEqual = True
+    for sol1 in sols1:
+        if not sol1 in sols2:
+            areEqual = False
+            print(sol1)
+    if not areEqual:
+        print("not in ip")
+    areEqual = True
+    for sol2 in sols2:
+        if not sol2 in sols1:
+            areEqual = False
+            print(sol2)
+    if not areEqual:
+        print("not in cp")
 
-# python compareSols.py <ipsol filename> <cpsol filename>
+# python compareSols.py <cpsol filename> <ipsol filename>
 ipfile = sys.argv[1]
 cpfile = sys.argv[2]
 
 ipsol = readSol(ipfile)
 cpsol = readSol(cpfile)
-areSame = compare(ipsol, cpsol)
-if not areSame[0]:
-    print(ipfile, cpfile, " are not the same, at least ", areSame[1:], " differ.")
+compare(ipsol, cpsol)
